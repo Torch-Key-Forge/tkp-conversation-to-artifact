d229fdbd364795c6c0862a2e29a3b5f36f6558b9
Carl Burke <smallplotnursery@gmail.com>
2026-07-18 19:34:41 -0400

    Add package initialization

---
+++ src/tkp_conversation_to_artifact/__init__.py
@@ -0,0 +1,6 @@
+"""TKP Conversation-to-Artifact composer."""
+
+from .composer import compose_artifacts, write_artifact_package
+
+__all__ = ["compose_artifacts", "write_artifact_package"]
+__version__ = "0.1.0"
